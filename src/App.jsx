7aca438fb4a7e0d32a8c4a2d4035dcadc06d08e1
Nico <nico@Nicos-MacBook-Pro.local>
2025-03-13 22:06:05 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <h1>Media Bank</h1>
+    </div>
+  )
+}
+
+export default App
